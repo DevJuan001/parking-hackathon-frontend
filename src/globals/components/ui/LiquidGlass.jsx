7372b7f41654a843e55c57d59ac693a6653224a1
DevJuan001 <juanesyt7@gmail.com>
2026-07-05 17:37:24 -0500

    feat: add LiquidGlass component for enhanced visual effects with SVG filter

---
+++ src/globals/components/ui/LiquidGlass.jsx
@@ -0,0 +1,81 @@
+import { useId } from "react";
+
+export default function LiquidGlass({ children, className, onClick }) {
+  const filterId = `liquid-glass-distort-${useId()}`;
+
+  return (
+    <>
+      <svg style={{ display: "none" }} xmlns="http://www.w3.org/2000/svg">
+        <filter
+          id={filterId}
+          x="0%"
+          y="0%"
+          width="100%"
+          height="100%"
+          filterUnits="objectBoundingBox"
+        >
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.01 0.015"
+            numOctaves="2"
+            result="noise"
+          />
+
+          <feFlood floodColor="white" result="whiteRect" />
+          <feMorphology
+            in="whiteRect"
+            operator="erode"
+            radius="0.2"
+            result="eroded"
+          />
+          <feComposite
+            in="whiteRect"
+            in2="eroded"
+            operator="out"
+            result="ring"
+          />
+          <feGaussianBlur in="ring" stdDeviation="0.2" result="blurredRing" />
+
+          <feComposite
+            in="noise"
+            in2="blurredRing"
+            operator="arithmetic"
+            k1="0.6"
+            k2="0.6"
+            k3="0.6"
+            k4="0.6"
+            result="modulatedNoise"
+          />
+
+          <feDisplacementMap
+            in="SourceGraphic"
+            in2="modulatedNoise"
+            scale="20"
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        </filter>
+      </svg>
+
+      <div
+        onClick={onClick}
+        className={`relative ${className}`}
+        style={{ backdropFilter: `url(#${filterId})` }}
+      >
+        <div
+          className="pointer-events-none absolute w-full h-full rounded-full bg-linear-to-b from-white/30 to-white/5 
+          dark:from-white/8 dark:to-black/30"
+        />
+
+        <div
+          className="pointer-events-none absolute w-full h-full rounded-full shadow-[0px_0px_1.5px_0.1px_#999999] ring ring-inset ring-white/20
+          dark:ring-white/10"
+        />
+
+        <div className="relative w-full h-full flex items-center justify-center">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
