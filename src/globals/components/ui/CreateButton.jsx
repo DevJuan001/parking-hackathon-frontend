044a6e42e20d009b4d512612863788fd4e191968
DevJuan001 <juanesyt7@gmail.com>
2026-07-08 18:41:18 -0500

    refactor(CreateButton): enhance button component by adding default borderRadius and improving icon weight and styling

--- src/globals/components/ui/CreateButton.jsx
+++ src/globals/components/ui/CreateButton.jsx
@@ -1,21 +1,33 @@
 import Icon from "@components/ui/Icon";
 
-export default function CreateButton({ icon, text, borderRadius, onClick }) {
+export default function CreateButton({
+  icon,
+  text,
+  borderRadius = "40px",
+  onClick,
+}) {
   return (
     <button
       onClick={onClick}
       style={{ borderRadius: borderRadius }}
-      className={`flex items-center ${text ? "px-5" : "px-3.5"} py-5 gap-2 bg-black font-poppins transition duration-500
-      lg:rounded-3xl lg:py-3
+      className={`flex items-center ${text ? "px-5" : "px-3"} py-5 gap-2 bg-black font-poppins transition duration-500
+      lg:py-3
       hover:shadow-lg
       dark:bg-white dark:hover:shadow-[0px_0px_32px_-11px_#ffffff]`}
     >
-      <Icon name={icon ? icon : "add"} weight={600} className="invert" />
+      <Icon
+        name={icon ? icon : "add"}
+        weight={500}
+        className="invert
+        dark:invert-0"
+      />
 
       {text && (
         <span
           data-flip-id="modal-title"
-          className="hidden text-white font-medium dark:text-black lg:block"
+          className="hidden text-white font-medium
+          lg:block
+          dark:text-black"
         >
           {text}
         </span>
