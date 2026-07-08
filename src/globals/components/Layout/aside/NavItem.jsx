15399ca99ae393f49ad2716e191a455f97252928
DevJuan001 <juanesyt7@gmail.com>
2026-07-08 18:27:36 -0500

    refactor(NavItem): optimize styles and transitions for improved performance and visual consistency

--- src/globals/components/Layout/aside/NavItem.jsx
+++ src/globals/components/Layout/aside/NavItem.jsx
@@ -10,43 +10,51 @@ export default function NavItem({
   onClick,
 }) {
   return (
-    <NavLink
-      to={path}
-      className={"transition-all duration-700"}
-      onClick={onClick}
-    >
+    <NavLink to={path} onClick={onClick}>
       {({ isActive }) => (
         <div
           id={`${itemId}-aside-button`}
-          className={`w-auto h-14 flex items-center py-3 px-6 gap-2 subpixel-antialiased rounded-full transition-all duration-500 group
+          className={`w-auto h-14 flex items-center py-3 px-6 gap-2 subpixel-antialiased rounded-full transition-[background-color,color,box-shadow] duration-300 group
             ${
               isActive
                 ? `bg-black shadow-[0px_0px_18px_-11px_#000000] text-white
-              dark:bg-white dark:text-black dark:shadow-[0px_0px_18px_-11px_#ffffff] animate-clickEffect`
-                : `text-[#75777E]
+              dark:bg-white dark:text-black dark:shadow-[0px_0px_18px_-11px_#ffffff]`
+                : `bg-transparent text-[#75777E]
               hover:bg-[#e5e7eb96]
-              dark:text-[#75777eb7] dark:hover:bg-[#181818]`
+              dark:bg-transparent dark:text-[#75777eb7] dark:hover:bg-[#181818]`
             }`}
         >
           <Icon
             name={icon}
             size={25}
             fill={isActive}
-            className={`transition-colors duration-700 ease-in-out
+            className={`transition-colors duration-300 ease-in-out
             ${
               isActive
-                ? "group-hover:scale-105 dark:fill-black"
-                : "text-[#75777eb7] fill-none group-hover:text-black group-hover:[--icon-weight:500] dark:group-hover:text-white"
+                ? `group-hover:scale-105
+                dark:fill-black`
+                : `text-[#75777eb7]
+                group-hover:text-black group-hover:[--icon-weight:500] dark:group-hover:text-white`
             }`}
           />
 
           <div
-            className={`text-center font-medium overflow-hidden transition-all duration-500 ease-in-out
-            ${showName ? "max-w-[200px]" : "max-w-0 group-hover:max-w-[200px]"}
-            ${isActive ? "font-semibold max-w-[200px]" : "group-hover:text-black dark:group-hover:text-white"}`}
+            className={`text-center font-medium overflow-hidden transition-[max-width,opacity] duration-700 ease-in-out
+            ${
+              showName
+                ? `max-w-[200px] opacity-100`
+                : `max-w-0 opacity-0
+                group-hover:max-w-[200px] group-hover:opacity-100`
+            }
+            ${
+              isActive
+                ? `font-semibold max-w-[200px] opacity-100`
+                : `group-hover:text-black
+                dark:group-hover:text-white`
+            }`}
           >
             <span
-              className={`inline-block text-nowrap whitespace-nowrap origin-left group-not-hover:animate-grow-out group-hover:animate-grow-in`}
+              className={`inline-block text-nowrap whitespace-nowrap origin-left`}
             >
               {name}
             </span>
