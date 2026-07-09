8b3a65054c0885cb284959d62d13556600b2d0d1
DevJuan001 <juanesyt7@gmail.com>
2026-07-08 21:47:38 -0500

    refactor(Routing): replace routesConfig with layoutRoutes and standAloneRoutes for improved route management and lazy loading

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,48 @@
-import ProtectedRoutes from "@/router/ProtectedRoutes";
-import { routesConfig } from "@/router/constants/routesConfig";
-import LandingPage from "@/modules/landing/LandingPage";
+import { Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+// Constantes
+import { layoutRoutes, standAloneRoutes } from "@/router/constants/routes";
+// Componentes
+import ProtectedRoutes from "@/router/ProtectedRoutes";
+import LandingPage from "@/modules/landing/LandingPage";
 import Layout from "@/globals/components/Layout/Layout";
 
 export default function AppRouter() {
   return (
     <Routes>
-      {/* Ruta no existente lo que hace es enviarlo al login */}
       <Route path="*" element={<Navigate to="/" />} />
 
-      {/* Página Login o de Inicio de Sesión */}
       <Route path="/" element={<LandingPage />} />
 
       <Route element={<Layout />}>
-        {routesConfig.map(({ path, component: Component, roles }) => (
-          <Route key={path} element={<ProtectedRoutes roles={roles} />}>
-            <Route path={path} element={<Component />} />
-          </Route>
-        ))}
+        {layoutRoutes.map(
+          ({ path, component: Component, roles, loading: Fallback }) => (
+            <Route key={path} element={<ProtectedRoutes roles={roles} />}>
+              <Route
+                path={path}
+                element={
+                  <Suspense fallback={<Fallback />}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            </Route>
+          ),
+        )}
       </Route>
+
+      {standAloneRoutes.map(({ path, component: Component, roles }) => (
+        <Route key={path} element={<ProtectedRoutes roles={roles} />}>
+          <Route
+            path={path}
+            element={
+              <Suspense fallback={null}>
+                <Component />
+              </Suspense>
+            }
+          />
+        </Route>
+      ))}
     </Routes>
   );
 }
---
+++ src/router/constants/routes.js
@@ -0,0 +1,82 @@
+import { lazy } from "react";
+// Loadings
+import ParkingLoading from "@/modules/parking/components/ui/ParkingLoading";
+
+// Pages
+const HomePage = lazy(() => import("@/modules/home/HomePage"));
+const UsersPage = lazy(() => import("@/modules/users/UsersPage"));
+const ExitsPage = lazy(() => import("@/modules/exits/ExitsPage"));
+const EntriesPage = lazy(() => import("@/modules/entries/EntriesPage"));
+const ParkingPage = lazy(() => import("@/modules/parking/ParkingPage"));
+const FinancePage = lazy(() => import("@/modules/finance/FinancePage"));
+const CheckInPage = lazy(() => import("@/modules/check-in/CheckInPage"));
+const OnBoardingPage = lazy(
+  () => import("@/modules/on-boarding/OnBoardingPage"),
+);
+const VehiclePayment = lazy(
+  () => import("@/modules/vehicle-payment/VehiclePayment"),
+);
+
+export const layoutRoutes = [
+  {
+    path: "/home",
+    component: HomePage,
+    loading: ParkingLoading,
+    roles: ["Admin"],
+  },
+
+  {
+    path: "/users",
+    component: UsersPage,
+    loading: ParkingLoading,
+    roles: ["Admin"],
+  },
+
+  {
+    path: "/entries",
+    component: EntriesPage,
+    loading: ParkingLoading,
+    roles: ["Admin"],
+  },
+
+  {
+    path: "/parking",
+    component: ParkingPage,
+    roles: ["Admin"],
+    loading: ParkingLoading,
+  },
+
+  {
+    path: "/exits",
+    component: ExitsPage,
+    loading: ParkingLoading,
+    roles: ["Admin"],
+  },
+
+  {
+    path: "/finance",
+    component: FinancePage,
+    loading: ParkingLoading,
+    roles: ["Admin"],
+  },
+];
+
+export const standAloneRoutes = [
+  {
+    path: "/on-boarding",
+    component: OnBoardingPage,
+    roles: ["Admin"],
+  },
+
+  {
+    path: "/vehicle-payment",
+    component: VehiclePayment,
+    roles: ["Cliente"],
+  },
+
+  {
+    path: "/check-in",
+    component: CheckInPage,
+    roles: ["Cliente"],
+  },
+];
