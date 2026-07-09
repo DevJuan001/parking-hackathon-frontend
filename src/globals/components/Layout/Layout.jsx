770832901ae74764e01534ff04432bbabadf8079
DevJuan001 <juanesyt7@gmail.com>
2026-07-08 21:45:36 -0500

    refactor(Layout): adjust padding in layout component for improved visual consistency

--- src/globals/components/Layout/Layout.jsx
+++ src/globals/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Aside from "@components/Layout/aside/Aside";
 export default function Layout() {
   return (
     // Container
-    <div className="w-screen h-screen flex flex-col py-3 px-6">
+    <div className="w-screen h-screen flex flex-col py-2.5 px-4">
       <Outlet />
 
       <Aside />
