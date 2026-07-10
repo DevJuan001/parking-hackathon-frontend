a09ac374c74deab82bdd037511ea12bc0dbb378b
DevJuan001 <juanesyt7@gmail.com>
2026-07-09 22:18:50 -0500

    feat(parking): integrate infinite scrolling in SpotsPanel and add ref support in SpotItem

--- src/modules/parking/components/ui/SpotItem.jsx
+++ src/modules/parking/components/ui/SpotItem.jsx
@@ -1,11 +1,12 @@
 import Icon from "@components/ui/Icon";
 import { spotStatus } from "@/modules/parking/constants/spotStatus";
 
-export default function SpotItem({ name, status = 2, icon, onClick }) {
+export default function SpotItem({ ref, name, status = 2, icon, onClick }) {
   const config = spotStatus[status] || spotStatus[2];
 
   return (
     <button
+      ref={ref}
       onClick={onClick}
       className={`h-full w-full flex flex-col items-center justify-center gap-1 rounded-3xl transition-colors duration-200 overflow-hidden text-ellipsis
       ${config.styles}`}
--- src/modules/parking/components/ui/SpotsPanel.jsx
+++ src/modules/parking/components/ui/SpotsPanel.jsx
@@ -1,6 +1,7 @@
 // Hooks
 import { useSpots } from "@/modules/parking/hooks/useSpots";
 import { useFloors } from "@/modules/parking/hooks/useFloors";
+import { useInfiniteScroll } from "@/globals/hooks/useInfiniteScroll";
 import { useFilterSpots } from "@/modules/parking/hooks/useFilterSpots";
 // Constants
 import { vehicleTypesConstant } from "@/globals/constants/vehicleTypes";
@@ -14,19 +15,32 @@ import CreateButton from "@components/ui/CreateButton";
 
 export default function SpotsPanel({ openModal }) {
   const { floors } = useFloors();
-  const { spots, loading, filters, setFilters } = useSpots();
+  const {
+    spots,
+    loading,
+    hasNextPage,
+    isFetchingNextPage,
+    filters,
+    fetchNextPage,
+    setFilters,
+  } = useSpots();
   const { handleChange } = useFilterSpots(filters, setFilters);
+  const { getItemRef } = useInfiniteScroll({
+    items: spots,
+    hasNextPage,
+    fetchNextPage,
+  });
   const noSpots = spots?.length === 0 && !loading;
   const isFirstLoad = spots?.length === 0 && loading;
 
   return (
     <section
-      className="h-full w-full px-7 py-6 flex flex-col gap-1 col-span-1 row-span-2 border-3 border-[#EBE6E7] rounded-[50px]
+      className="h-full w-full px-6.5 py-6 flex flex-col gap-1 col-span-1 row-span-2 border-3 border-[#EBE6E7] rounded-[50px]
       dark:text-white dark:border-[#202022]"
     >
       <div className="flex items-center justify-between">
         <span className="font-semibold">Plazas</span>
-        
+
         <SelectMenu
           id={"floors-menu"}
           name={"floor_id"}
@@ -75,13 +89,14 @@ export default function SpotsPanel({ openModal }) {
       ) : (
         !noSpots && (
           <div
-            className="h-full w-full grid grid-cols-8 grid-rows-7 pt-2 gap-2
+            className="h-full w-full grid grid-cols-8 auto-rows-[100px] pt-2 p-0.5 gap-2 scrollbar-thumb-[#E4E2E5] overflow-hidden overflow-y-auto
             md:grid-cols-4
             lg:grid-cols-6 lg:gap-x-2 lg:gap-y-4
             2xl:grid-cols-8 2xl:gap-2.5"
           >
-            {spots?.map((spot) => (
+            {spots?.map((spot, index) => (
               <SpotItem
+                ref={getItemRef(index)}
                 key={spot.spot_id}
                 name={spot.spot}
                 status={spot.spot_status}
@@ -90,13 +105,17 @@ export default function SpotsPanel({ openModal }) {
               />
             ))}
 
+            {isFetchingNextPage && (
+              <Skeleton count={2} width="100%" height={"100%"} />
+            )}
+
             <button
               onClick={(e) =>
                 openModal(filters.floor_id, "createSpot", e.currentTarget)
               }
-              className="h-full w-full flex flex-col items-center justify-center gap-2 border-2 border-[#EBE6E7] rounded-3xl transition-colors 
-              hover:bg-[#efedf0] 
-              dark:hover:bg-[#ffffff15] dark:border-3 dark:border-[#202022]"
+              className="h-full w-full flex flex-col items-center justify-center gap-2 rounded-3xl border-2 border-[#E4E2E5] transition-colors 
+              hover:bg-[#efedf0] hover:cursor-pointer
+              dark:hover:bg-[#5f585815] dark:border-3 dark:border-[#202022]"
             >
               <Icon name={"add"} size={35} />
             </button>
