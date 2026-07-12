3dcd43749715ee8f4100937a996d91ad0cc8e047
DevJuan001 <juanesyt7@gmail.com>
2026-07-12 16:39:24 -0500

    style(UsersKpis): enhance layout for improved responsiveness and alignment

--- src/modules/users/components/ui/UsersKpis.jsx
+++ src/modules/users/components/ui/UsersKpis.jsx
@@ -5,7 +5,10 @@ export default function UsersKpis() {
   const { stats } = useUserStats();
 
   return (
-    <section className="w-full flex items-center gap-4">
+    <section
+      className="w-full flex flex-wrap items-center justify-center gap-2
+      md:flex-nowrap md:gap-4"
+    >
       <Kpi title={"Recientes"} value={stats.created_this_week ?? 0} />
 
       <Kpi title={"Activos"} value={stats.active ?? 0} />
