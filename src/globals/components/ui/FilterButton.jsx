6167349736856476f93d33ca66def922266e3b34
DevJuan001 <juanesyt7@gmail.com>
2026-07-12 15:59:50 -0500

    style(FilterButton): update border color and text for improved accessibility and consistency

--- src/globals/components/ui/FilterButton.jsx
+++ src/globals/components/ui/FilterButton.jsx
@@ -5,16 +5,25 @@ export default function FilterButton({ onClick, filterButtonVisibility }) {
     <button
       id="filter-button"
       onClick={onClick}
-      className={`flex items-center px-5 py-2.5 gap-2 rounded-3xl bg-[#FBF9FC] transition-all duration-200 border border-gray-200
+      className={`flex items-center px-5 py-2.5 gap-2 rounded-3xl bg-[#FBF9FC] transition-all duration-200 border border-[#E4E2E5]
         hover:rounded-[40px]
         hover:bg-gray-300
         dark:bg-[#101012] dark:hover:bg-[#38383b9f] dark:shadow-none dark:border-none
         ${filterButtonVisibility ? "block" : "hidden"}`}
     >
-      <Icon name={"page_info"} size={24} className="dark:text-[#C5C6CE]" />
+      <Icon
+        data-shared-id="filter-icon"
+        name={"page_info"}
+        size={24}
+        className="dark:text-[#C5C6CE]"
+      />
 
-      <span className="hidden font-medium dark:text-[#C5C6CE] lg:block">
-        Filtrar
+      <span
+        data-shared-id="filter-text"
+        className="font-medium
+        dark:text-[#C5C6CE]"
+      >
+        Filtros
       </span>
     </button>
   );
