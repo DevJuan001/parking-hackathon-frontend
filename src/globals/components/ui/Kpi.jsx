f5305950b1a2cc0707cabf5eb540bb1b4c9893a7
DevJuan001 <juanesyt7@gmail.com>
2026-07-12 15:29:10 -0500

    feat(Kpi): introduce new Kpi component and remove UsersKpi component

---
+++ src/globals/components/ui/Kpi.jsx
@@ -0,0 +1,22 @@
+export default function Kpi({ title, value }) {
+  return (
+    <div
+      className="w-full flex flex-col items-start py-5 px-6 rounded-2xl bg-[#f5f3f6]
+      dark:bg-[#101012]"
+    >
+      <span
+        className="text-sm text-[#75777E]
+        dark:text-[#7E8088]"
+      >
+        {title}
+      </span>
+
+      <span
+        className="text-2xl font-medium
+        dark:text-[#E4E2E5]"
+      >
+        {value}
+      </span>
+    </div>
+  );
+}
