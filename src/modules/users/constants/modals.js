6584149b53690b17d101b095763b312a67bf0df2
DevJuan001 <juanesyt7@gmail.com>
2026-07-12 15:29:38 -0500

    feat(modals): add user-related modal constants for edit, create, enable, disable, and filter actions

---
+++ src/modules/users/constants/modals.js
@@ -0,0 +1,7 @@
+export const modals = {
+  editUser: { title: "Editar Usuario", location: "center" },
+  createUser: { title: "Crear Usuario", location: "center" },
+  disableUser: { title: "Deshabilitar Usuario" },
+  enableUser: { title: "Habilitar Usuario" },
+  filterUsers: { title: "Filtrar Usuarios" },
+};
