8222f0da8830ef4e15a64a80a9b98a193a84baf0
DevJuan001 <juanesyt7@gmail.com>
2026-07-13 18:29:18 -0500

    feat(CreateButton): add miniVersion prop for responsive button sizing in FloorsPanel

--- src/globals/components/ui/CreateButton.jsx
+++ src/globals/components/ui/CreateButton.jsx
@@ -1,6 +1,7 @@
 import Icon from "@components/ui/Icon";
 
 export default function CreateButton({
+  miniVersion = false,
   icon,
   text,
   borderRadius = "40px",
@@ -10,7 +11,7 @@ export default function CreateButton({
     <button
       onClick={onClick}
       style={{ borderRadius: borderRadius }}
-      className={`flex items-center ${text ? "p-5" : "py-3 px-5"} gap-2 bg-black font-poppins transition duration-500
+      className={`flex items-center ${text ? "px-5 py-3" : miniVersion ? "p-3" : "p-5"} gap-2 bg-black font-poppins transition duration-500
       focus:animate-click-effect
       hover:shadow-lg
       dark:bg-white dark:hover:shadow-[0px_0px_32px_-11px_#ffffff]`}
