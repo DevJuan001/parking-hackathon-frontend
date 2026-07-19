94f7a5a5047ab3cdd6ae4ad74ca8b4ceac302ae3
DevJuan001 <juanesyt7@gmail.com>
2026-07-18 22:17:03 -0500

    feat(useChat): implement custom chat hook for managing chat state and message sending functionality

---
+++ src/globals/hooks/useChat.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useFormValidation } from "@hooks/useFormValidation";
+import { sendMessageService } from "../services/sendMessageService";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export function useChat() {
+  const [chatInfo, setChatInfo] = useState({
+    message: "",
+  });
+  const [error, setError] = useState(null);
+  const { validate } = useFormValidation();
+  const queryClient = useQueryClient();
+
+  function handleChange(e) {
+    setChatInfo((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const { data: messages = [] } = useQuery({
+    queryKey: ["chat"],
+    queryFn: () => [],
+    initialData: [],
+    staleTime: 1000 * 60 * 60,
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (chatInfo) => sendMessageService(chatInfo),
+    onMutate: () => {
+      queryClient.setQueryData(["chat"], (prev = []) => [
+        ...prev,
+        {
+          id: Date.now(),
+          role: "user",
+          content: chatInfo.message,
+        },
+      ]);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["chat"], (prev = []) => [
+        ...prev,
+        {
+          id: Date.now() + 1,
+          role: "assistant",
+          content: data.data.response,
+        },
+      ]);
+    },
+    onError: () => {
+      setError(
+        "Lo sentimos ha ocurrido un error, intenta enviar el mensaje nuevamente.",
+      );
+      queryClient.setQueryData(["chat"], (prev = []) => [
+        ...prev,
+        {
+          id: Date.now() + 1,
+          role: "assistant",
+          content:
+            "Lo sentimos ha ocurrido un error, intenta enviar el mensaje nuevamente.",
+        },
+      ]);
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const isValid = validate(chatInfo);
+
+    if (!isValid) {
+      return;
+    }
+
+    mutate(chatInfo);
+
+    setChatInfo({
+      message: "",
+    });
+  }
+
+  return {
+    messages,
+    chatInfo,
+    isPending,
+    error,
+    handleChange,
+    handleSubmit,
+  };
+}
