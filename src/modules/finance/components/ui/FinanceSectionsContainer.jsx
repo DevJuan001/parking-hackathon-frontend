77d6299eaaadce0debd98ed8d34b41eed2ee38d5
DevJuan001 <juanesyt7@gmail.com>
2026-07-20 21:52:14 -0500

    feat(FinanceSectionsContainer): integrate useExitsStats hook and update KPI components for dynamic data display

--- src/modules/finance/components/ui/FinanceSectionsContainer.jsx
+++ src/modules/finance/components/ui/FinanceSectionsContainer.jsx
@@ -1,3 +1,5 @@
+// Hooks
+import { useExitsStats } from "@/modules/exits/hooks/useExitsStats";
 // Componentes
 import BalanceKpi from "@/modules/finance/components/ui/BalanceKpi";
 import IncomesKpi from "@/modules/finance/components/ui/IncomesKpi";
@@ -5,17 +7,20 @@ import ExpensesKpi from "@/modules/finance/components/ui/ExpensesKpi";
 import IncomesChart from "@/modules/finance/components/ui/IncomesChart";
 import HistorialSection from "@/modules/finance/components/ui/HistorialSection";
 
-export default function FinanceSectionsContainer() {
+export default function FinanceSectionsContainer({ openModal }) {
+  const { stats } = useExitsStats();
+
   return (
     <section
-      className="w-full h-[90%] grid grid-cols-3 grid-rows-5 gap-3 font-dmsans
+      className="w-full h-[85%] grid grid-cols-1 grid-rows-[150px_150px_150px_300px] gap-3 font-dmsans
+      md:h-[90%] md:grid-cols-3 md:grid-rows-5
       dark:text-[#E4E2E5]"
     >
-      <BalanceKpi />
+      <BalanceKpi incomes={stats?.total_revenue || 0} expenses={80105} />
 
-      <IncomesKpi />
+      <IncomesKpi incomes={stats?.total_revenue || 0} />
 
-      <ExpensesKpi />
+      <ExpensesKpi expenses={4000} />
 
       <IncomesChart />
 
