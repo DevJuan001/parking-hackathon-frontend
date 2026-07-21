87e74c13ca518572a84ef6d04c15fd5fb0cc9244
DevJuan001 <juanesyt7@gmail.com>
2026-07-20 21:51:15 -0500

    feat(BalanceKpi): enhance component with dynamic balance calculation and modal integration

--- src/modules/finance/components/ui/BalanceKpi.jsx
+++ src/modules/finance/components/ui/BalanceKpi.jsx
@@ -1,7 +1,17 @@
-export default function BalanceKpi() {
+// Hooks
+import { useInnerModal } from "@hooks/useInnerModal";
+// Componentes
+import Icon from "@components/ui/Icon";
+import Calendar from "@components/ui/Calendar";
+
+export default function BalanceKpi({ incomes, expenses }) {
+  const balancePercentage = Math.round(((incomes - expenses) / incomes) * 100);
+  const { innerType, innerTrigger, openInnerModal, closeInnerModal } =
+    useInnerModal();
+
   return (
     <div
-      className="flex flex-col p-5 gap-2 rounded-4xl bg-black text-[#E4E2E5]
+      className="relative flex flex-col p-5 gap-2 rounded-4xl bg-black text-[#E4E2E5]
       dark:bg-white dark:text-black"
     >
       <span className="text-lg">Balance</span>
@@ -11,11 +21,42 @@ export default function BalanceKpi() {
           className="text-5xl text-white font-medium
           dark:text-black"
         >
-          $4.000
+          ${incomes - expenses}
         </span>
 
-        <span className="ml-1.5 text-xs">COP</span>
+        <span className="ml-1.5">COP</span>
       </div>
+
+      <div className="flex items-center">
+        <Icon
+          name={balancePercentage ? "arrow_upward_alt" : "arrow_downward_alt"}
+          weight={500}
+        />
+
+        <span>{balancePercentage}% rentabilidad</span>
+      </div>
+
+      <button
+        onClick={(e) => openInnerModal("calendar", e)}
+        className="absolute right-3.5 bottom-3.5 w-12 h-12 flex items-center justify-center rounded-4xl bg-white
+        hover:bg-white/90
+        active:animate-click-effect"
+      >
+        <Icon
+          name={"calendar_month"}
+          className={`text-black
+          dark:text-white`}
+          weight={600}
+        />
+      </button>
+
+      {innerType === "calendar" && (
+        <Calendar
+          growDirection={"anchored"}
+          triggerRef={innerTrigger}
+          onClose={closeInnerModal}
+        />
+      )}
     </div>
   );
 }
