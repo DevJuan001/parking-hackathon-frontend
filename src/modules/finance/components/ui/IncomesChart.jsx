5008cd21ed65215c26b05fbf235b3af2fccef81f
DevJuan001 <juanesyt7@gmail.com>
2026-07-20 21:50:40 -0500

    feat(IncomesChart): integrate custom hook for dynamic income data and loading state management

--- src/modules/finance/components/ui/IncomesChart.jsx
+++ src/modules/finance/components/ui/IncomesChart.jsx
@@ -1,25 +1,20 @@
-import SelectMenu from "@components/modals/SelectMenu";
+// Hooks
+import { useIncomesChartData } from "@/modules/finance/hooks/useIncomesChartData";
+// Componentes
+import Icon from "@components/ui/Icon";
+import SelectMenu from "@modals/SelectMenu";
+import Skeleton from "@components/ui/Skeleton";
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
 
 export default function IncomesChart() {
-  const thisYearData = [
-    { label: "Enero", value: 4 },
-    { label: "Febrero", value: 12 },
-    { label: "Marzo", value: 22 },
-    { label: "Abril", value: 22 },
-    { label: "Mayo", value: 22 },
-    { label: "Junio", value: 22 },
-    { label: "Julio", value: 22 },
-    { label: "Agosto", value: 4 },
-    { label: "Octubre", value: 15 },
-    { label: "Septiembre", value: 22 },
-    { label: "Noviembre", value: 15 },
-    { label: "Diciembre", value: 10 },
-  ];
+  const { incomesData, form, loading, handleChange, handleSubmit } =
+    useIncomesChartData();
+  const noIncomes = incomesData.length === 0 && !loading;
 
   return (
     <div
-      className="col-span-2 row-span-4 flex flex-col p-5 gap-2 rounded-4xl border border-[#E4E2E5]
+      className="flex flex-col p-5 gap-2 rounded-4xl border border-[#E4E2E5]
+      md:col-span-2 md:row-span-4 
       dark:border-[#17171a]"
     >
       <div className="flex justify-between">
@@ -27,20 +22,56 @@ export default function IncomesChart() {
 
         <SelectMenu
           miniVersion
-          options={[{ value: 1, label: "Este año" }]}
-          value={1}
+          name={"period"}
+          value={form.period}
+          onChange={handleChange}
+          optionOnClick={handleSubmit}
+          options={[
+            { value: "1y", label: "Este año" },
+            { value: "1m", label: "Este mes" },
+            { value: "1w", label: "Esta semana" },
+          ]}
         />
       </div>
 
-      <ResponsiveContainer>
-        <BarChart responsive data={thisYearData}>
-          <Bar dataKey={"value"} radius={[42, 42, 0, 0]} fill="var(--color-chart-bar)" />
+      {noIncomes && (
+        <div
+          className="h-full w-full flex flex-col items-center justify-center gap-2 text-[#75777E]
+          dark:text-[#7E8088]"
+        >
+          <Icon name={"border_clear"} size={90} />
 
-          <XAxis dataKey={"label"} style={{ fontSize: "14px" }} />
+          <span className="text-xl font-semibold font-poppins">
+            No se encontraron ingresos registrados
+          </span>
+        </div>
+      )}
 
-          <Tooltip cursor={false} />
-        </BarChart>
-      </ResponsiveContainer>
+      {loading ? (
+        <Skeleton
+          width="100%"
+          height="100%"
+          backgroundColor={"#F3EEF5"}
+          darkModeBackgroundColor={"#101012"}
+          shineColor="#C5C1C7"
+          darkModeShineColor="#1e1e1e"
+          borderRadius={"50px"}
+        />
+      ) : (
+        <ResponsiveContainer>
+          <BarChart responsive data={incomesData}>
+            <Bar
+              dataKey={"value"}
+              radius={[42, 42, 0, 0]}
+              fill="var(--color-chart-bar)"
+            />
+
+            <XAxis dataKey={"label"} style={{ fontSize: "14px" }} />
+
+            <Tooltip cursor={false} />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
     </div>
   );
 }
