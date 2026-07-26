3deb1d5b0a971286eaec3b73fab302b5e0e673bd
DevJuan001 <juanesyt7@gmail.com>
2026-07-25 23:41:36 -0500

    refactor(FinanceSectionsContainer): replace useExitsStats with props for incomes and filters, enhancing component flexibility

--- src/modules/finance/components/ui/FinanceSectionsContainer.jsx
+++ src/modules/finance/components/ui/FinanceSectionsContainer.jsx
@@ -1,44 +1,43 @@
-// Hooks
-import { useExitsStats } from "@/modules/exits/hooks/useExitsStats";
 // Componentes
 import BalanceKpi from "@/modules/finance/components/ui/BalanceKpi";
 import IncomesKpi from "@/modules/finance/components/ui/IncomesKpi";
 import ExpensesKpi from "@/modules/finance/components/ui/ExpensesKpi";
 import IncomesChart from "@/modules/finance/components/ui/IncomesChart";
-import FinanceLoading from "@/modules/finance/components/ui/FinanceLoading";
 import HistorialSection from "@/modules/finance/components/ui/HistorialSection";
 
-export default function FinanceSectionsContainer() {
-  const { stats, loading } = useExitsStats();
-
+export default function FinanceSectionsContainer({
+  incomes,
+  filters,
+  setFilters,
+}) {
   return (
     <section className="w-full h-[90%]">
-      {loading && <FinanceLoading />}
-
-      {stats && (
+      {incomes && (
         <div
           className="w-full h-full grid grid-cols-1 grid-rows-[150px_150px_150px_300px] gap-3 font-dmsans
           md:grid-cols-3 md:grid-rows-5
-          dark:text-[#E4E2E5]"
+          dark:text-[#E4E2E5]z"
         >
           <BalanceKpi
-            incomes={stats?.total_revenue || 0}
-            expenses={stats?.expenses || 0}
+            incomes={incomes?.total_revenue || 0}
+            expenses={incomes?.expenses || 0}
+            filters={filters}
+            setFilters={setFilters}
           />
 
           <IncomesKpi
-            incomes={stats?.total_revenue || 0}
-            lastMonthIncomes={stats?.last_month_incomes || 0}
+            incomes={incomes?.this_month_revenue || 0}
+            lastMonthIncomes={incomes?.last_month_incomes || 0}
           />
 
           <ExpensesKpi
-            expenses={stats?.expenses || 0}
-            lastMonthExpenses={stats?.last_month_expenses || 0}
+            expenses={incomes?.this_month_expenses || 0}
+            lastMonthExpenses={incomes?.last_month_expenses || 0}
           />
 
           <IncomesChart />
 
-          <HistorialSection expenses={stats?.expenses || 0} />
+          <HistorialSection />
         </div>
       )}
     </section>
