339e97b13c4ea26b343d26c5fa18f53575caec5d
DevJuan001 <juanesyt7@gmail.com>
2026-07-25 23:41:01 -0500

    feat(FinancePage): integrate loading state and exits stats into FinanceSectionsContainer

--- src/modules/finance/FinancePage.jsx
+++ src/modules/finance/FinancePage.jsx
@@ -1,17 +1,20 @@
 // Hooks
 import { useModal } from "@hooks/useModal";
+import { useExitsStats } from "@/modules/exits/hooks/useExitsStats";
 // Constantes
 import { modals } from "@/modules/finance/constants/modals";
 // Componentes
 import TopSection from "@components/ui/TopSection";
+import FinanceLoading from "@/modules/finance/components/ui/FinanceLoading";
+import FinanceSectionsContainer from "@/modules/finance/components/ui/FinanceSectionsContainer";
 // Modales
 import Modal from "@modals/Modal";
 import ExportModal from "@modals/ExportModal";
 import FilterFinanceModal from "@/modules/finance/components/modals/FilterFinanceModal";
-import FinanceSectionsContainer from "@/modules/finance/components/ui/FinanceSectionsContainer";
 
 export default function FinancePage() {
   const { isOpen, modalType, triggerRef, openModal, closeModal } = useModal();
+  const { stats, loading, filters, setFilters } = useExitsStats();
 
   return (
     <main className="w-full h-full overflow-hidden overflow-y-auto">
@@ -21,7 +24,15 @@ export default function FinancePage() {
         filterButtonOnClick={(e) => openModal(null, "filter", e.currentTarget)}
       />
 
-      <FinanceSectionsContainer />
+      {loading ? (
+        <FinanceLoading inside />
+      ) : (
+        <FinanceSectionsContainer
+          incomes={stats}
+          filters={filters}
+          setFilters={setFilters}
+        />
+      )}
 
       {modalType && (
         <Modal
