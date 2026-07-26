5fb22a2deaa161e0fef42a5f0a347e13dd62dc78
DevJuan001 <juanesyt7@gmail.com>
2026-07-25 23:41:15 -0500

    feat(FinanceLoading): enhance loading component with dynamic height and updated skeleton layout

--- src/modules/finance/components/ui/FinanceLoading.jsx
+++ src/modules/finance/components/ui/FinanceLoading.jsx
@@ -1,11 +1,12 @@
 import Skeleton from "@components/ui/Skeleton";
 
-export default function FinanceLoading() {
+export default function FinanceLoading({ inside }) {
   return (
     <div
-      className="w-full h-full grid grid-cols-1 grid-rows-[150px_150px_150px_300px] gap-3 mt-18 font-dmsans
+      className={`w-full grid grid-cols-1 grid-rows-[150px_150px_150px_300px] gap-3  font-dmsans
+      ${inside ? "h-[90%]" : "h-full mt-18 mb-5"}
       md:grid-cols-3 md:grid-rows-5
-      dark:text-[#E4E2E5]"
+      dark:text-[#E4E2E5]`}
     >
       <Skeleton
         width="100%"
@@ -17,16 +18,6 @@ export default function FinanceLoading() {
         borderRadius={"32px"}
       />
 
-      <Skeleton 
-        width="100%"
-        height="100%"
-        backgroundColor={"#F3EEF5"}
-        darkModeBackgroundColor={"#101012"}
-        shineColor="#C5C1C7"
-        darkModeShineColor="#1e1e1e"
-        borderRadius={"32px"}
-      />
-
       <Skeleton
         width="100%"
         height="100%"
@@ -45,7 +36,6 @@ export default function FinanceLoading() {
         shineColor="#C5C1C7"
         darkModeShineColor="#1e1e1e"
         borderRadius={"32px"}
-        className="col-span-2 row-span-2"
       />
 
       <Skeleton
@@ -56,7 +46,18 @@ export default function FinanceLoading() {
         shineColor="#C5C1C7"
         darkModeShineColor="#1e1e1e"
         borderRadius={"32px"}
-        className="row-span-2"
+        className="col-span-2 row-span-4"
+      />
+
+      <Skeleton
+        width="100%"
+        height="100%"
+        backgroundColor={"#F3EEF5"}
+        darkModeBackgroundColor={"#101012"}
+        shineColor="#C5C1C7"
+        darkModeShineColor="#1e1e1e"
+        borderRadius={"32px"}
+        className="row-span-4"
       />
     </div>
   );
