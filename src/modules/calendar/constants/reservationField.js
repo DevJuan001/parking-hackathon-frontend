a815912b626e96c4f648f12c9d119c395c323019
DevJuan001 <juanesyt7@gmail.com>
2026-07-29 22:19:37 -0500

    fix(calendar): update dark mode styles in reservationField for improved visibility

--- src/modules/calendar/constants/reservationField.js
+++ src/modules/calendar/constants/reservationField.js
@@ -2,12 +2,12 @@ export const reservationField = {
   1: {
     styles: `bg-blue-50 border-cyan-300 text-start
     hover:bg-[#E4E2E5]
-    dark:bg-blue-800/30 dark:hover:bg-[#202022]`,
+    dark:bg-blue-800/30 dark:hover:bg-[#28282B]`,
   },
 
   2: {
     styles: `bg-black text-white text-start
     hover:bg-black/80
-    dark:bg-white dark:text-black`,
+    dark:bg-white dark:text-black dark:hover:bg-white/90`,
   },
 };
