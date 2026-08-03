1b651711f8199a879978178941b26843a1f9c18d
DevJuan001 <juanesyt7@gmail.com>
2026-08-02 21:20:44 -0500

    feat(TopSection): integrate inner modal functionality and enhance layout for improved mobile options

--- src/modules/calendar/components/ui/TopSection.jsx
+++ src/modules/calendar/components/ui/TopSection.jsx
@@ -1,7 +1,12 @@
+// Hooks
+import { useInnerModal } from "@hooks/useInnerModal";
+// Componentes
 import Icon from "@components/ui/Icon";
 import ShareButton from "@components/ui/ShareButton";
 import CreateButton from "@components/ui/CreateButton";
 import ExportButton from "@components/ui/ExportButton";
+// Modales
+import Modal from "@/globals/components/modals/Modal";
 
 export default function TopSection({
   text,
@@ -11,6 +16,9 @@ export default function TopSection({
   setActiveCalendarLayout,
   openModal,
 }) {
+  const { innerType, innerTrigger, openInnerModal, closeInnerModal } =
+    useInnerModal();
+
   return (
     <div
       className="h-[13%] flex items-center justify-between
@@ -18,74 +26,97 @@ export default function TopSection({
       md:h-[8%]
       dark:text-[#E4E2E5]"
     >
-      <div className="h-full flex items-center gap-3">
+      <div className="h-full w-full flex items-center justify-between gap-3">
         <div className="flex items-center gap-3">
           <button
             onClick={prevButtonOnClick}
-            className="w-fit h-fit flex items-center justify-center p-2.5 rounded-3xl
+            className="w-fit h-fit flex items-center justify-center p-1 rounded-3xl
+            md:p-2.5
             hover:bg-[#EFEDF0]
             dark:hover:bg-[#101012]"
           >
             <Icon name={"keyboard_arrow_left"} />
           </button>
 
-          <span className="text-2xl font-medium font-poppins">{text}</span>
+          <span
+            className="text-xl text-nowrap font-medium font-dmsans
+            md:text-2xl"
+          >
+            {text}
+          </span>
 
           <button
             onClick={nextButtonOnClick}
-            className="w-fit h-fit flex items-center justify-center text-center p-2.5 rounded-3xl
+            className="w-fit h-fit flex items-center justify-center p-1 rounded-3xl
+            md:p-2.5
             hover:bg-[#EFEDF0]
             dark:hover:bg-[#101012]"
           >
             <Icon name={"keyboard_arrow_right"} />
           </button>
+
+          <div
+            className="hidden h-fit py-1.5 px-3.5 gap-1 bg-[#F5F3F6] rounded-4xl
+            md:flex
+            dark:bg-[#101012]"
+          >
+            <button
+              onClick={() => setActiveCalendarLayout("dayLayout")}
+              className="h-full flex items-center justify-center p-2"
+            >
+              <Icon
+                name={"calendar_view_day"}
+                fill={activeCalendarLayout === "dayLayout" ? true : false}
+                className="text-[#75777E]
+                dark:text-[#7E8088]"
+              />
+            </button>
+
+            <button
+              onClick={() => setActiveCalendarLayout("weekLayout")}
+              className="flex items-center justify-center p-2"
+            >
+              <Icon
+                name={"calendar_view_week"}
+                fill={activeCalendarLayout === "weekLayout" ? true : false}
+                className="text-[#75777E]
+                dark:text-[#7E8088]"
+              />
+            </button>
+
+            <button
+              onClick={() => setActiveCalendarLayout("monthLayout")}
+              className="h-full flex items-center justify-center p-2"
+            >
+              <Icon
+                name={"calendar_view_month"}
+                fill={activeCalendarLayout === "monthLayout" ? true : false}
+                className="text-[#75777E]
+                dark:text-[#7E8088]"
+              />
+            </button>
+          </div>
         </div>
 
-        <div
-          className="h-fit flex py-1.5 px-3.5 gap-1 bg-[#F5F3F6] rounded-4xl
-          dark:bg-[#101012]"
+        <button
+          onClick={(e) => openInnerModal("topSectionMobileOptions", e)}
+          className="flex items-center justify-center p-4.5 rounded-4xl border border-[#E4E2E5]
+          focus:animate-click-effect
+          hover:bg-[#F5F3F6]
+          md:hidden"
         >
-          <button
-            onClick={() => setActiveCalendarLayout("dayLayout")}
-            className="h-full flex items-center justify-center p-2"
-          >
-            <Icon
-              name={"calendar_view_day"}
-              fill={activeCalendarLayout === "dayLayout" ? true : false}
-              className="text-[#75777E]
-              dark:text-[#7E8088]"
-            />
-          </button>
-
-          <button
-            onClick={() => setActiveCalendarLayout("weekLayout")}
-            className="flex items-center justify-center p-2"
-          >
-            <Icon
-              name={"calendar_view_week"}
-              fill={activeCalendarLayout === "weekLayout" ? true : false}
-              className="text-[#75777E]
-              dark:text-[#7E8088]"
-            />
-          </button>
-
-          <button
-            onClick={() => setActiveCalendarLayout("monthLayout")}
-            className="h-full flex items-center justify-center p-2"
-          >
-            <Icon
-              name={"calendar_view_month"}
-              fill={activeCalendarLayout === "monthLayout" ? true : false}
-              className="text-[#75777E]
-              dark:text-[#7E8088]"
-            />
-          </button>
-        </div>
+          <Icon
+            name={"more_horiz"}
+            className="text-[#75777E]
+            dark:text-[#7E8088]"
+          />
+        </button>
       </div>
 
       <div
-        className="flex gap-2
-          lg:gap-3"
+        className="hidden gap-2
+        md:flex
+        lg:gap-3"
       >
         <ShareButton
           onClick={(e) => openModal(null, "share", e.currentTarget)}
@@ -99,6 +130,47 @@ export default function TopSection({
           onClick={(e) => openModal(null, "createReservation", e.currentTarget)}
         />
       </div>
+
+      {innerType === "topSectionMobileOptions" && (
+        <Modal
+          disableHeader
+          isOpen={true}
+          type={innerType}
+          triggerRef={innerTrigger}
+          onClose={closeInnerModal}
+        >
+          <div className="flex flex-col gap-1">
+            <button
+              onClick={(e) =>
+                openModal(null, "createReservation", e.currentTarget)
+              }
+              className="flex items-center p-4 gap-2 bg-[#F5F3F6] rounded-4xl"
+            >
+              <Icon name={"add"} />
+
+              <span>Crear reserva</span>
+            </button>
+
+            <button
+              onClick={(e) => openModal(null, "share", e.currentTarget)}
+              className="flex items-center p-4 gap-2 bg-[#F5F3F6] rounded-4xl"
+            >
+              <Icon name={"event_upcoming"} />
+
+              <span>Compartir</span>
+            </button>
+
+            <button
+              onClick={(e) => openModal(null, "export", e.currentTarget)}
+              className="flex items-center p-4 gap-2 bg-[#F5F3F6] rounded-4xl"
+            >
+              <Icon data-shared-id="export-icon" name={"download"} />
+
+              <span>Exportar</span>
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
