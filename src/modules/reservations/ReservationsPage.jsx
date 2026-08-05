0cc067be215ad07a3e641d1d4aa8a195a505fc97
DevJuan001 <juanesyt7@gmail.com>
2026-08-05 17:26:52 -0500

    feat(reservations): create ReservationsPage component with Navbar integration

---
+++ src/modules/reservations/ReservationsPage.jsx
@@ -0,0 +1,9 @@
+import Navbar from "@/modules/reservations/components/ui/Navbar";
+
+export default function ReservationPage() {
+  return (
+    <section className="w-full h-full flex flex-col font-dmsans">
+      <Navbar />
+    </section>
+  );
+}
