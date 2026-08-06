27b6b3c6f59b4efb26c2e3c99f73eac570b5f892
DevJuan001 <juanesyt7@gmail.com>
2026-08-06 10:43:07 -0500

    feat(reservations): integrate modal functionality in ReservationsPage with Login and HowItWorks modals

--- src/modules/reservations/ReservationsPage.jsx
+++ src/modules/reservations/ReservationsPage.jsx
@@ -1,9 +1,37 @@
-import Navbar from "@/modules/reservations/components/ui/Navbar";
+// Hooks
+import { useModal } from "@hooks/useModal";
+// Constantes
+import { modals } from "@/modules/reservations/constants/modals";
+// Modales
+import LoginModal from "@modals/LoginModal";
+import Modal from "@components/modals/Modal";
+import HowItWorks from "@/modules/reservations/components/modals/HowItWorksModal";
+import ReservationCard from "@/modules/reservations/components/ui/ReservationCard";
 
 export default function ReservationPage() {
+  const { isOpen, triggerRef, modalType, modalData, openModal, closeModal } =
+    useModal();
+
   return (
-    <section className="w-full h-full flex flex-col font-dmsans">
-      <Navbar />
+    <section className="w-full h-dvh flex items-center justify-center p-12 font-dmsans">
+      <ReservationCard openModal={openModal} />
+
+      {modalType && (
+        <Modal
+          isOpen={isOpen}
+          type={modalType}
+          triggerRef={triggerRef}
+          margin={modals[modalType]?.margin}
+          location={modals[modalType]?.location}
+          disableHeader={modals[modalType]?.disableHeader}
+          growDirection={modals[modalType]?.growDirection}
+          onClose={closeModal}
+        >
+          {modalType === "logIn" && <LoginModal />}
+
+          {modalType === "howItWorks" && <HowItWorks />}
+        </Modal>
+      )}
     </section>
   );
 }
