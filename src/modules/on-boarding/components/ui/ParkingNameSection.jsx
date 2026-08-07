c994bd708c6db05579c6e64375d0d4e44f6e9968
DevJuan001 <juanesyt7@gmail.com>
2026-08-06 21:43:00 -0500

    refactor(ParkingNameSection): remove unused activeSection prop and update layout and text styles for better responsiveness

--- src/modules/on-boarding/components/ui/ParkingNameSection.jsx
+++ src/modules/on-boarding/components/ui/ParkingNameSection.jsx
@@ -2,7 +2,6 @@ import FormField from "@components/ui/FormField";
 import SectionButtons from "@/modules/on-boarding/components/ui/SectionButtons";
 
 export default function ParkingNameSection({
-  activeSection,
   form,
   handleChange,
   fieldError,
@@ -11,29 +10,31 @@ export default function ParkingNameSection({
 }) {
   return (
     <section
-      className={`${
-        activeSection === "parkingName"
-          ? "h-full w-full flex flex-col items-center justify-center gap-5 animate-blur-down"
-          : "hidden"
-      } `}
+      className="h-full w-full flex flex-col items-center justify-center p-4 gap-5 animate-blur-down
+      lg:w-5xl"
     >
-      <div className="w-4xl flex flex-col items-center">
+      <div className="w-full flex flex-col items-center">
         <span
-          className="text-xl text-nowrap font-medium text-[#75777e]
+          className="text-sm text-nowrap font-medium text-[#75777e]
+          lg:text-xl
           dark:text-[#7E8088]"
         >
           Ahora configuremos la información de tu parqueadero.
         </span>
 
         <span
-          className="text-6xl font-semibold
+          className="text-[25px] font-semibold tracking-tighter
+          lg:text-5xl lg:tracking-normal
           dark:text-[#E4E2E5]"
         >
           ¿Cómo se llama tu parqueadero?
         </span>
       </div>
 
-      <form className="w-lg flex flex-col gap-2">
+      <form
+        className="w-full flex flex-col gap-2
+        md:w-lg"
+      >
         <FormField
           name={"parking_name"}
           labelText={"Nombre *"}
