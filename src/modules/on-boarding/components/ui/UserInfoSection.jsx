375229056690ba599d18e6677b47d76d7d32caad
DevJuan001 <juanesyt7@gmail.com>
2026-08-06 21:43:31 -0500

    refactor(UserInfoSection): remove unused activeSection prop and update layout and text styles for improved responsiveness

--- src/modules/on-boarding/components/ui/UserInfoSection.jsx
+++ src/modules/on-boarding/components/ui/UserInfoSection.jsx
@@ -1,8 +1,8 @@
+// Componentes
 import FormField from "@components/ui/FormField";
 import SectionButtons from "@/modules/on-boarding/components/ui/SectionButtons";
 
 export default function UserInfoSection({
-  activeSection,
   form,
   handleChange,
   fieldError,
@@ -10,29 +10,28 @@ export default function UserInfoSection({
 }) {
   return (
     <section
-      className={`${
-        activeSection === "userInfo"
-          ? "h-full w-full flex flex-col items-center justify-center gap-5 animate-blur-down"
-          : "hidden"
-      }`}
+      className="h-full w-full flex flex-col items-center justify-center p-4 gap-5 animate-blur-down
+      md:w-lg"
     >
-      <div className="w-lg flex flex-col">
+      <div className="w-full flex flex-col">
         <span
-          className="text-lg text-nowrap font-medium text-[#75777e]
+          className="text-sm font-medium text-[#75777e]
+          lg:text-lg lg:text-nowrap
           dark:text-[#7E8088]"
         >
           Estamos a pocos pasos de poner tu parqueadero en marcha.
         </span>
 
         <span
-          className="text-6xl font-semibold
+          className="text-4xl font-semibold
+          md:text-6xl
           dark:text-[#E4E2E5]"
         >
           ¿Cómo te llamas?
         </span>
       </div>
 
-      <form className="w-lg flex flex-col gap-2">
+      <form className="w-full flex flex-col gap-2">
         <FormField
           labelText={"Nombre *"}
           placeholder={"Escribe tu nombre aquí"}
