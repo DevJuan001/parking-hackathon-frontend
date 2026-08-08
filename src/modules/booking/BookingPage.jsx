6dc63fb1e5272d073ad4fc3f53633906e7aba8ba
DevJuan001 <juanesyt7@gmail.com>
2026-08-08 00:47:10 -0500

    feat(BookingPage): integrate reservation form handling and success state management with new UI components

--- src/modules/booking/BookingPage.jsx
+++ src/modules/booking/BookingPage.jsx
@@ -1,21 +1,57 @@
 // Hooks
+import { useState } from "react";
 import { useModal } from "@hooks/useModal";
+import { useCreateReservation } from "@/modules/booking/hooks/useCreateReservation";
 // Constantes
 import { modals } from "@/modules/booking/constants/modals";
 // Componentes
 import BookingCard from "@/modules/booking/components/ui/BookingCard";
-
+import SuccessReservation from "@/modules/booking/components/ui/SuccessReservation";
 // Modales
 import LoginModal from "@modals/LoginModal";
 import Modal from "@components/modals/Modal";
+import { useParkingInfo } from "./hooks/useParkingInfo";
 
 export default function BookingPage() {
-  const { isOpen, triggerRef, modalType, modalData, openModal, closeModal } =
-    useModal();
+  const { isOpen, triggerRef, modalType, openModal, closeModal } = useModal();
+  const [activeSection, setActiveSection] = useState("card");
+  const {
+    form,
+    loading,
+    error,
+    setForm,
+    handleChange,
+    handleSubmit,
+    fieldError,
+  } = useCreateReservation(setActiveSection);
+  const { parkingInfo } = useParkingInfo();
 
   return (
-    <section className="w-full h-full flex items-center justify-center font-dmsans">
-      <BookingCard openModal={openModal} />
+    <section
+      className="w-screen h-screen flex items-center justify-center font-dmsans
+      md:p-10"
+    >
+      {activeSection === "card" && (
+        <BookingCard
+          form={form}
+          error={error}
+          loading={loading}
+          setForm={setForm}
+          openModal={openModal}
+          fieldError={fieldError}
+          parkingInfo={parkingInfo}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      )}
+
+      {activeSection === "success" && (
+        <SuccessReservation
+          form={form}
+          setForm={setForm}
+          setActiveSection={setActiveSection}
+        />
+      )}
 
       {modalType && (
         <Modal
