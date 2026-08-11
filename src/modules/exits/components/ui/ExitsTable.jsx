45959b9bfb303fea4d382914491cacb888b601fb
DevJuan001 <juanesyt7@gmail.com>
2026-08-10 20:43:40 -0500

    style(ExitsTable): update dark mode background and increase icon size for better visibility

--- src/modules/exits/components/ui/ExitsTable.jsx
+++ src/modules/exits/components/ui/ExitsTable.jsx
@@ -15,10 +15,10 @@ export default function ExitsTable({ exits, loading }) {
     >
       {noExits && (
         <div
-          className="h-[700px] flex flex-col items-center justify-center gap-1 rounded-2xl text-[#7E8088] bg-[#f5f3f6]
-          dark:text-[#E4E2E5] dark:bg-black"
+          className="h-full flex flex-col items-center justify-center gap-1 rounded-2xl text-[#7E8088] bg-[#f5f3f6]
+          dark:text-[#E4E2E5] dark:bg-[#101012]"
         >
-          <Icon name={"border_clear"} size={64} />
+          <Icon name={"border_clear"} size={72} />
 
           <span
             className="text-xl font-medium text-center
