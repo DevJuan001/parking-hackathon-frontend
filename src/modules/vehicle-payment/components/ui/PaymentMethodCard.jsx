3c90b15071e91c247ac07855e1bdad287153e62c
DevJuan001 <juanesyt7@gmail.com>
2026-08-13 20:54:00 -0500

    style(PaymentMethodCard): enhance button styles for improved dark mode support and layout consistency

--- src/modules/vehicle-payment/components/ui/PaymentMethodCard.jsx
+++ src/modules/vehicle-payment/components/ui/PaymentMethodCard.jsx
@@ -1,5 +1,7 @@
-import Icon from "@components/ui/Icon";
+// Constantes
 import { paymentMethodStyles } from "@/modules/vehicle-payment/constants/paymentMethodStyles";
+// Componentes
+import Icon from "@components/ui/Icon";
 
 export default function PaymentMethodCard({
   id,
@@ -12,9 +14,18 @@ export default function PaymentMethodCard({
     <button
       id={`payment-method-${id}`}
       onClick={onClick}
-      className={`h-32 w-44 flex flex-col items-center justify-center rounded-2xl p-5 gap-1 border border-[#ffffff99] shadow outline-0
+      className={`h-32 w-[48%] flex flex-col items-center justify-center p-5 gap-1 rounded-2xl leading-4.5 font-medium border border-[#ffffff99] shadow outline-0
+      active:animate-click-effect
       hover:scale-[1.02]
-      ${activePaymentMethod === id ? "bg-[#00000015]" : "bg-[#ffffff3d] hover:bg-[#00000006]"}
+      dark:border-[#101012]
+      ${
+        activePaymentMethod === id
+          ? `bg-[#00000015]
+          dark:bg-[#303032]`
+          : `bg-[#ffffff3d] 
+          hover:bg-[#00000006]
+          dark:bg-[#101012] dark:hover:bg-[#1d1d20ee]`
+      }
       `}
     >
       <Icon
@@ -24,7 +35,7 @@ export default function PaymentMethodCard({
         className={paymentMethodStyles[text]?.styles}
       />
 
-      <span className="font-medium">{text}</span>
+      <span>{text}</span>
     </button>
   );
 }
