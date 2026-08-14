7a02baab0ffde279adf04c5c0a2c60c11fc04b07
DevJuan001 <juanesyt7@gmail.com>
2026-08-13 20:54:27 -0500

    style(SuccessPaymentSection): enhance dark mode support with updated styles for text and background

--- src/modules/vehicle-payment/components/ui/SuccessPaymentSection.jsx
+++ src/modules/vehicle-payment/components/ui/SuccessPaymentSection.jsx
@@ -10,11 +10,23 @@ export default function SuccessPaymentSection({
   }, 5000);
 
   return (
-    <div className="self-center h-full flex flex-col justify-center gap-2 animate-blur-down">
-      <div className="w-20 h-20 flex items-center justify-center bg-green-200 rounded-full">
+    <div
+      className="self-center h-full flex flex-col justify-center gap-2 animate-blur-down
+      dark:text-[#E4E2E5]"
+    >
+      <div
+        className="w-20 h-20 flex items-center justify-center bg-green-200 rounded-full
+        dark:bg-green-950"
+      >
         <Icon name={"check"} size={50} color={"#008236"} />
       </div>
-      <span className="text-3xl text-[#75777E]">¡Gracias por elegirnos!</span>
+
+      <span
+        className="text-3xl text-[#75777E]
+        dark:text-[#7E8088]"
+      >
+        ¡Gracias por elegirnos!
+      </span>
 
       <span className="text-6xl font-semibold">
         Pago registrado correctamente
