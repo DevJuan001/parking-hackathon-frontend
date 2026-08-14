b3b23b7182f28ab65fd70d29c6514d79d309c079
DevJuan001 <juanesyt7@gmail.com>
2026-08-13 20:19:11 -0500

    refactor(CreateEntrySection): restructure form layout and enhance styling for dark mode support

--- src/modules/check-in/components/ui/CreateEntrySection.jsx
+++ src/modules/check-in/components/ui/CreateEntrySection.jsx
@@ -17,8 +17,14 @@ export default function CreateEntrySection({
     useInnerModal();
 
   return (
-    <section className="w-full h-full flex flex-col">
-      <div className="self-center justify-self-center h-full flex flex-col items-center justify-center gap-6 animate-blur-down">
+    <section
+      className="w-full h-full flex flex-col
+      dark:text-[#E4E2E5]"
+    >
+      <form
+        onSubmit={(e) => handleSubmit(e, openInnerModal)}
+        className="self-center justify-self-center h-full flex flex-col items-center justify-center gap-6 animate-blur-down"
+      >
         <div className="w-full flex flex-col gap-1">
           <span className="text-3xl text-start text-[#7E777E]">Bienvenido</span>
 
@@ -36,31 +42,37 @@ export default function CreateEntrySection({
         </div>
 
         <input
-          type="text"
           autoFocus
-          placeholder="ABC123"
+          type="text"
           name="plate"
+          maxLength={6}
           autoComplete="off"
-          value={entryData.plate || ""}
+          placeholder="ABC123"
+          value={entryData?.plate ?? ""}
           onChange={handleChange}
           className="w-full h-44 px-3 rounded-3xl bg-[#00000008] text-7xl text-center font-semibold outline-0
-            placeholder:text-[#1b1b1e52]"
-          maxLength={6}
+          placeholder:text-[#1b1b1e52]
+          dark:bg-[#101012] dark:placeholder:text-[#c5c3c65d]"
         />
 
         <button
+          type="submit"
           onClick={(e) => handleSubmit(e, openInnerModal)}
-          className="w-full py-5 rounded-3xl bg-black text-white font-semibold
-            hover:bg-black/90"
+          className="w-full py-5 rounded-3xl bg-black text-lg text-white font-semibold
+          active:animate-click-effect
+          hover:bg-black/90
+          dark:bg-white dark:text-black dark:hover:bg-white/95"
         >
           {loading ? <Loader /> : "Enviar"}
         </button>
-      </div>
+      </form>
 
       <a
         href="/vehicle-payment"
-        className="self-end justify-self-end px-12 py-3 rounded-4xl bg-[#00000008] text-nowrap font-semibold transition-colors duration-300 outline-0
-        hover:bg-[#00000018]"
+        className="self-end justify-self-end px-12 py-3 rounded-4xl bg-[#00000008] text-nowrap font-semibold transition-colors outline-0
+        active:animate-click-effect
+        hover:bg-[#00000018]
+        dark:bg-[#101012]"
       >
         Ir a Pagar
       </a>
