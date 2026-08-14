8e80971f74c276d48e803730882a2c7e93df71a3
DevJuan001 <juanesyt7@gmail.com>
2026-08-13 20:42:34 -0500

    style(CreateEntrySection): improve text styling and dark mode support for better readability

--- src/modules/check-in/components/ui/CreateEntrySection.jsx
+++ src/modules/check-in/components/ui/CreateEntrySection.jsx
@@ -26,11 +26,19 @@ export default function CreateEntrySection({
         className="self-center justify-self-center h-full flex flex-col items-center justify-center gap-6 animate-blur-down"
       >
         <div className="w-full flex flex-col gap-1">
-          <span className="text-3xl text-start text-[#7E777E]">Bienvenido</span>
+          <span
+            className="text-3xl text-start text-[#75777E]
+            dark:text-[#7E8088]"
+          >
+            Bienvenido
+          </span>
 
           <div className="flex items-center gap-2">
-            <span className="text-6xl font-semibold">
-              Escribe tu placa aqui
+            <span
+              className="text-5xl font-semibold
+              md:text-6xl"
+            >
+              Escribe tu placa aquí
             </span>
 
             <Icon
@@ -58,8 +66,9 @@ export default function CreateEntrySection({
         <button
           type="submit"
           onClick={(e) => handleSubmit(e, openInnerModal)}
-          className="w-full py-5 rounded-3xl bg-black text-lg text-white font-semibold
+          className="w-full py-5 rounded-3xl bg-black text-white font-semibold
           active:animate-click-effect
+          md:text-lg
           hover:bg-black/90
           dark:bg-white dark:text-black dark:hover:bg-white/95"
         >
@@ -72,7 +81,7 @@ export default function CreateEntrySection({
         className="self-end justify-self-end px-12 py-3 rounded-4xl bg-[#00000008] text-nowrap font-semibold transition-colors outline-0
         active:animate-click-effect
         hover:bg-[#00000018]
-        dark:bg-[#101012]"
+        dark:bg-[#101012] dark:hover:bg-[#202022]"
       >
         Ir a Pagar
       </a>
