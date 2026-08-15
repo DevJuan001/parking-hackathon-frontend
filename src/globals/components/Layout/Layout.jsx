8c946c6a56d3156589e76ec822beea0e4639aa45
DevJuan001 <juanesyt7@gmail.com>
2026-08-14 22:14:22 -0500

    style(Layout): add overflow hidden to layout container for improved content management

--- src/globals/components/Layout/Layout.jsx
+++ src/globals/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Aside from "@components/Layout/aside/Aside";
 export default function Layout() {
   return (
     // Container
-    <div className="relative w-dvw h-dvh flex flex-col py-2.5 px-4">
+    <div className="relative w-dvw h-dvh flex flex-col py-2.5 px-4 overflow-hidden">
       <Outlet />
 
       <Aside />
