2e8d328257aa1d56c134bb0abf6f07de75b00d33
DevJuan001 <juanesyt7@gmail.com>
2026-08-16 16:51:37 -0500

    refactor(ProfileModal): enhance profile modal structure by integrating dynamic sections and improving layout

--- src/globals/components/modals/profileModal/ProfileModal.jsx
+++ src/globals/components/modals/profileModal/ProfileModal.jsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { useInnerModal } from "@hooks/useInnerModal";
 import { useCurrentUser } from "@hooks/useCurrentUser";
+// Constantes
+import { items } from "@constants/profileModalItems";
 // Components
 import Icon from "@components/ui/Icon";
 import LiquidGlass from "@components/ui/LiquidGlass";
@@ -11,11 +13,12 @@ import SubscriptionContent from "@modals/profileModal/SubscriptionContent";
 // Modals
 import Modal from "@modals/Modal";
 import EditInfoModal from "@modals/profileModal/EditInfoModal";
+import ParkingInfoContent from "@modals/profileModal/ParkingInfoContent";
 import ChangePasswordModal from "@modals/profileModal/ChangePasswordModal";
 
 export default function ProfileModal({ triggerRef, onCloseModal }) {
-  const { user } = useCurrentUser();
-  const [activeSection, setActiveSection] = useState("general");
+  const { user, hasRole } = useCurrentUser();
+  const [activeSection, setActiveSection] = useState("General");
   const { innerTrigger, innerType, openInnerModal, closeInnerModal } =
     useInnerModal();
 
@@ -31,109 +34,73 @@ export default function ProfileModal({ triggerRef, onCloseModal }) {
     >
       <section
         className="h-full flex flex-col-reverse items-center justify-between gap-4
-        md:grid md:grid-cols-[150px_1fr]"
+        md:grid md:grid-cols-[180px_1fr]"
       >
         <aside
-          className="h-[15%] w-full justify-self-end mt-3 font-dmsans border-gray-300
+          className="h-[15%] w-full justify-self-end p-1 font-dmsans border-gray-300
           md:h-full md:justify-self-start md:self-start
           dark:border-[#3a3d43]"
         >
-          {/* Lista de opciones */}
           <ul
             className="flex justify-center gap-1
             md:flex-col md:justify-start"
           >
-            <li
-              onClick={() => setActiveSection("general")}
-              className={`w-full flex flex-col items-center py-2.5 px-3 rounded-2xl gap-2 text-xs transition
+            {items
+              ?.filter((item) => hasRole(item?.roles))
+              .map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => setActiveSection(item?.name)}
+                  className={`w-full flex flex-col items-center py-2.5 px-3 rounded-2xl gap-2 text-xs transition
                 hover:cursor-pointer
                 md:flex-row md:w-full md:text-sm
                 ${
-                  activeSection === "general"
+                  activeSection === item?.name
                     ? `bg-[#F5F3F6] font-medium text-[#44474e]
                       dark:text-white dark:bg-[#202022] dark:hover:bg-[#28282b]`
                     : `text-[#68676786] hover:bg-[#efedf0]
                       dark:hover:bg-[#101012] dark:hover:text-[#ffffff8a]`
                 }`}
-            >
-              <Icon name={"apps"} size={24} />
+                >
+                  <Icon
+                    name={item?.icon}
+                    size={24}
+                    fill={activeSection === item?.name ? true : false}
+                    animateFill
+                  />
 
-              <span>General</span>
-            </li>
-
-            <li
-              onClick={() => setActiveSection("appearance")}
-              className={`w-full flex flex-col items-center py-2.5 px-3 rounded-2xl gap-2 transition
-              hover:bg-[#efedf0] hover:cursor-pointer
-              md:flex-row md:pr-0 md:pl-3
-              dark:hover:bg-[#202022]
-              ${
-                activeSection === "appearance"
-                  ? `bg-[#F5F3F6] font-medium text-[#44474e]
-                    dark:text-white dark:bg-[#202022] dark:hover:bg-[#28282b]`
-                  : `text-[#68676786] hover:bg-[#efedf0]
-                    dark:hover:bg-[#101012] dark:hover:text-[#ffffff8a]`
-              }`}
-            >
-              <Icon
-                name={"palette"}
-                size={24}
-                fill={activeSection === "appearance" ? true : false}
-              />
-
-              <span
-                className="text-xs 
-                md:text-sm"
-              >
-                Apariencia
-              </span>
-            </li>
-
-            <li
-              onClick={() => setActiveSection("subscription")}
-              className={`w-full flex flex-col items-center py-2.5 px-3 rounded-2xl gap-2 transition
-              hover:bg-[#efedf0] hover:cursor-pointer
-              md:flex-row md:pr-0 md:pl-3
-              dark:hover:bg-[#202022]
-              ${
-                activeSection === "subscription"
-                  ? `bg-[#F5F3F6] font-medium text-[#44474e]
-                    dark:text-white dark:bg-[#202022] dark:hover:bg-[#28282b]`
-                  : `text-[#68676786] hover:bg-[#efedf0]
-                    dark:hover:bg-[#101012] dark:hover:text-[#ffffff8a]`
-              }`}
-            >
-              <Icon
-                name={"savings"}
-                size={24}
-                fill={activeSection === "subscription" ? true : false}
-              />
-
-              <span
-                className="text-xs 
-                md:text-sm"
-              >
-                Suscripción
-              </span>
-            </li>
+                  <span>{item?.name}</span>
+                </li>
+              ))}
           </ul>
         </aside>
 
         <section
-          className="relative h-full w-full flex flex-col px-2 gap-2 animate-blur-up
+          className="relative h-full w-full flex flex-col gap-2 animate-blur-up overflow-hidden overflow-y-auto
           md:h-full
           dark:text-white"
         >
-          <LiquidGlass
-            onClick={onCloseModal}
-            className="absolute h-11.5 mt-3 mr-1 self-end p-2.5 rounded-full
-            hover:cursor-pointer hover:bg-[#49454f21]"
-          >
-            <Icon name={"close"} />
-          </LiquidGlass>
+          {activeSection !== "Suscripción" && (
+            <div
+              className="h-fit flex items-center justify-between p-2
+              md:hidden"
+            >
+              <span className="text-lg font-medium font-dmsans">
+                Configuración
+              </span>
+
+              <LiquidGlass
+                onClick={onCloseModal}
+                className="flex h-11 p-2.5 rounded-full
+              hover:cursor-pointer hover:bg-[#49454f21]"
+              >
+                <Icon name={"close"} />
+              </LiquidGlass>
+            </div>
+          )}
 
           {/* Contenido de la sección seleccionada */}
-          {activeSection === "general" && (
+          {activeSection === "General" && (
             <GeneralContent
               user={user}
               openInnerModal={openInnerModal}
@@ -146,9 +113,11 @@ export default function ProfileModal({ triggerRef, onCloseModal }) {
             />
           )}
 
-          {activeSection === "appearance" && <AppearanceContent />}
+          {activeSection === "Parking" && <ParkingInfoContent />}
 
-          {activeSection === "subscription" && <SubscriptionContent />}
+          {activeSection === "Apariencia" && <AppearanceContent />}
+
+          {activeSection === "Suscripción" && <SubscriptionContent />}
 
           {/* Modales Internas */}
           {innerType === "editInfo" && (
