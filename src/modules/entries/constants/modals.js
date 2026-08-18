1c4b1ca373b909d644516c4f920c02c42c497f70
DevJuan001 <juanesyt7@gmail.com>
2026-08-18 10:17:19 -0500

    style(EntriesPage): enhance modal configurations with custom styles and margin handling

--- src/modules/entries/constants/modals.js
+++ src/modules/entries/constants/modals.js
@@ -1,9 +1,24 @@
 export const modals = {
   createEntry: { title: "Registrar Ingreso", location: "center" },
 
-  filter: { title: "Filtrar", disableHeader: true },
+  filter: {
+    title: "Filtrar",
+    disableHeader: true,
+    growDirection: "bottom-center",
+    styles: `p-5 h-auto w-[365px] rounded-[32px]
+    sm:w-[400px]`,
+  },
 
-  export: { disableHeader: true },
+  export: {
+    disableHeader: true,
+    growDirection: "center",
+    styles: `p-2 w-[300px] h-auto rounded-[32px]
+    md:w-[350px]`,
+  },
 
-  search: { disableHeader: true },
+  search: {
+    disableHeader: true,
+    growDirection: "left",
+    styles: `h-18 w-[350px] border-2 border-[#E4E2E5] rounded-3xl`,
+  },
 };
