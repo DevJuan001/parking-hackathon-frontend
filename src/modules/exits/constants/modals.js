f6bdcbcdda5a2052c00fe3cfec26d783590f810b
DevJuan001 <juanesyt7@gmail.com>
2026-08-18 10:17:26 -0500

    style(ExitsPage): enhance modal configurations with additional styles and margin handling

--- src/modules/exits/constants/modals.js
+++ src/modules/exits/constants/modals.js
@@ -1,9 +1,24 @@
 export const modals = {
   createExit: { title: "Registrar Salida", location: "center" },
 
-  export: { disableHeader: true },
+  filter: {
+    title: "Filtrar",
+    disableHeader: true,
+    growDirection: "bottom-center",
+    styles: `p-5 h-auto w-[365px] rounded-[32px]
+    sm:w-[400px]`,
+  },
 
-  search: { disableHeader: true },
+  export: {
+    disableHeader: true,
+    growDirection: "center",
+    styles: `p-2 w-[300px] h-auto rounded-[32px]
+    md:w-[350px]`,
+  },
 
-  filter: { disableHeader: true },
+  search: {
+    disableHeader: true,
+    growDirection: "left",
+    styles: `h-18 w-[350px] border-2 border-[#E4E2E5] rounded-3xl`,
+  },
 };
