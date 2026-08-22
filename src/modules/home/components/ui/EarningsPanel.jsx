fe8362322f61f37fb9b3f97a9ae775636cc98921
DevJuan001 <juanesyt7@gmail.com>
2026-08-22 00:19:17 -0500

    style(EarningsPanel): update layout styles for improved structure and responsiveness

--- src/modules/home/components/ui/EarningsPanel.jsx
+++ src/modules/home/components/ui/EarningsPanel.jsx
@@ -3,7 +3,7 @@ import RevenueChart from "@/modules/home/components/charts/RevenueChart";
 export default function EarningsPanel() {
   return (
     <div
-      className="h-full w-full px-7 py-6 rounded-[50px] border border-[#EBE6E7]
+      className="h-full w-full flex flex-col px-7 py-6 gap-2 rounded-[50px] border border-[#EBE6E7] overflow-hidden
       dark:text-white dark:border-[#202022]"
     >
       <span className="font-semibold">Ganancias</span>
