1f4875bc88d78acd11a0d524b054059de4a37a3a
DevJuan001 <juanesyt7@gmail.com>
2026-08-22 23:19:40 -0500

    refactor(user services): update import paths for utility functions to improve module resolution

--- src/modules/users/services/getAllUsersService.js
+++ src/modules/users/services/getAllUsersService.js
@@ -1,6 +1,6 @@
 import { apiRoutes } from "@/config/apiRoutes";
-import { buildQueryParams } from "@/utils/buildQueryParams";
-import { fetchWithAuth } from "@/utils/fetchWithAuth";
+import { buildQueryParams } from "@utils/buildQueryParams";
+import { fetchWithAuth } from "@utils/fetchWithAuth";
 
 export async function getAllUsersService({ pageParam = 1, filters = {} }) {
   const params = buildQueryParams({ ...filters, page: pageParam });
