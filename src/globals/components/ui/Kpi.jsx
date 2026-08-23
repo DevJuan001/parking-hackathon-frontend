b483336e6b8480523e92b0db1d5efc724ca593b7
DevJuan001 <juanesyt7@gmail.com>
2026-08-23 16:40:33 -0500

    refactor(Kpi): update layout and styling for improved responsiveness and typography consistency

--- src/globals/components/ui/Kpi.jsx
+++ src/globals/components/ui/Kpi.jsx
@@ -1,12 +1,12 @@
 export default function Kpi({ title, value }) {
   return (
     <div
-      className="w-[48.8%] flex flex-col items-start py-5 px-6 rounded-3xl bg-[#f5f3f6]
+      className="w-full flex flex-1 grow basis-32 flex-col items-start px-6 py-5.5 rounded-3xl bg-[#f5f3f6] font-dmsans
       md:w-full
       dark:bg-[#101012]"
     >
       <span
-        className="text-sm text-[#75777E]
+        className="text-[#75777E]
         dark:text-[#7E8088]"
       >
         {title}
