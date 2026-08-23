19322e69f539e3a7a2b8a591a4bb0395fd9b53b9
DevJuan001 <juanesyt7@gmail.com>
2026-08-23 16:48:48 -0500

    refactor(Buttons): update button padding for improved visual consistency across Export, Filter, and Search buttons

--- src/globals/components/ui/SearchButton.jsx
+++ src/globals/components/ui/SearchButton.jsx
@@ -4,7 +4,7 @@ export default function SearchButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center px-5 py-3 gap-2 rounded-2xl border border-[#E2E4E5]
+      className="flex items-center justify-center p-5 gap-2 rounded-2xl border border-[#E2E4E5]
       focus:animate-click-effect
       hover:bg-[#F5F3F6]
       dark:bg-black dark:hover:bg-[#101012] dark:border-[#202022]"
