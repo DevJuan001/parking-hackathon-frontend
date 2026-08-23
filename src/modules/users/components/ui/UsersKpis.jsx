eeca5eb788c8d6c8c5dc3a2cdda7971553284280
DevJuan001 <juanesyt7@gmail.com>
2026-08-23 16:50:55 -0500

    refactor(UsersKpis): add loading state with skeletons for improved user experience during data fetching

--- src/modules/users/components/ui/UsersKpis.jsx
+++ src/modules/users/components/ui/UsersKpis.jsx
@@ -1,21 +1,42 @@
+// Hooks
+import Skeleton from "@/globals/components/ui/Skeleton";
 import { useUserStats } from "@/modules/users/hooks/useUserStats";
-import Kpi from "@/globals/components/ui/Kpi";
+// Componentes
+import Kpi from "@components/ui/Kpi";
+import { Fragment } from "react";
 
 export default function UsersKpis() {
-  const { stats } = useUserStats();
+  const { stats, loading } = useUserStats();
 
   return (
     <section
       className="w-full flex flex-wrap items-center justify-center gap-2
-      md:flex-nowrap md:gap-4"
+      md:flex-nowrap"
     >
-      <Kpi title={"Recientes"} value={stats.created_this_week ?? 0} />
+      {loading ? (
+        Array.from({ length: 4 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            width="48%"
+            height={"100px"}
+            borderRadius={"20px"}
+            backgroundColor={"#F3EEF5"}
+            darkModeBackgroundColor={"#101012"}
+            shineColor="#C5C1C7"
+            darkModeShineColor="#1e1e1e"
+          />
+        ))
+      ) : (
+        <Fragment>
+          <Kpi title={"Recientes"} value={stats.created_this_week ?? 0} />
 
-      <Kpi title={"Activos"} value={stats.active ?? 0} />
+          <Kpi title={"Activos"} value={stats.active ?? 0} />
 
-      <Kpi title={"Deshabilitados"} value={stats.disabled ?? 0} />
+          <Kpi title={"Deshabilitados"} value={stats.disabled ?? 0} />
 
-      <Kpi title={"Total"} value={stats.total ?? 0} />
+          <Kpi title={"Total"} value={stats.total ?? 0} />
+        </Fragment>
+      )}
     </section>
   );
 }
